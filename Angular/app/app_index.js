f79276499687560a717dbfdd25d89d3f7b1892c0
Alex Flores <alexjf14@gmail.com>
2016-01-20 16:49:46 -0600

    Coneccion a la api local
    Aleluya se ha conectado...

--- Angular/app/app_index.js
+++
@@ -1,9 +0,0 @@
-angular.module("IndexApp",["ngRoute","ngResource"])
-
-.config(function($routeProvider){
-	$routeProvider
-		.when("/",{
-			controller: "indexController",
-			templateUrl: "templates/index/formas.html"
-		})
-});
--- Angular/controllers/controller_index.js
+++ Angular/controllers/controller_index.js
@@ -1,7 +1,29 @@
-angular.module("IndexApp",[])
 
-.controller("indexController",function($scope,$resource){
-	Post = $resource("http://jsonplaceholder.typicode.com/posts/:id", {id: "@id"});
+	// Creación del módulo
+var IndexApp = angular.module('IndexApp', ['ngRoute']);
 
-	$scope.posts = Post.query(); 
+// Configuración de las rutas
+IndexApp.config(function($routeProvider) {
+
+    $routeProvider
+        .when('/', {
+            templateUrl : 'templates/inicio.html',
+            controller  : 'IndexController'
+        })
+ 
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+IndexApp.controller("IndexController",function($scope,$http){
+	$scope.posts=[];
+    $http.get('http://localhost:9000/Country/')
+	.success(function(data){
+		console.log(data);
+		$scope.posts = data;
+	})
+	.error(function(err){
+
+	});
 });
